07605e762ba2170acadadf50515ac64efc4e0cc7
dosadczuk <damian.osadczuk@gmail.com>
2022-09-14 13:10:29 +0200

    Rename import alias.

--- src/jvmMain/kotlin/notification/listener/SystemNotificationListener.kt
+++ src/jvmMain/kotlin/notification/listener/SystemNotificationListener.kt
@@ -3,13 +3,13 @@ package notification.listener
 import androidx.compose.ui.window.TrayState
 import notification.Notification
 import notification.NotificationListener
-import androidx.compose.ui.window.Notification as TrayNotification
+import androidx.compose.ui.window.Notification as SystemNotification
 
 class SystemNotificationListener(private val controller: TrayState) : NotificationListener {
 
     override fun push(notification: Notification) {
         controller.sendNotification(
-            TrayNotification("NosAlert", notification.message, TrayNotification.Type.Info)
+            SystemNotification("NosAlert", notification.message, SystemNotification.Type.Info)
         )
     }
 }
